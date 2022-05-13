317e0bcc2c8641a0331c921dc65a42d721ca8b8c
Bailey <dabailey107@gmail.com>
2022-05-13 22:45:50 +0900

    Delete Scripts/Hooks directory

--- Scripts/Hooks/DCS-Stopwatch-hook.lua
+++
@@ -1,4 +0,0 @@
-status, result =  pcall(function() local dcsSr=require('lfs');dofile(dcsSr.writedir()..[[Mods\Tech\DCS-Stopwatch\Scripts\DCS-Stopwatch-OverlayGameGUI.lua]]); end,nil) 
- if not status then
- 	net.log(result)
- end
